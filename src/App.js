57d8516a4202d864a62f1b661f54b848236b16bd
unknown <manahilshaikh8551@gmail.com>
2023-02-15 10:08:07 -0800

    todo list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import './App.css';
 // import { Home } from './component4/Home';
 // import { TodoCompo } from './TodoCompo';
-import {Form}  from './Component3/Form'
+// import {Form}  from './Component3/Form'
+import { TodoList } from './Component3/TodoList';
 
 function App() {
   return (
     <div className="App">
       {/* <TodoCompo /> */}
       {/* <Home/> */}
-      <Form/>
+      {/* <Form/> */}
       {/* <h1>This is h1 tag</h1> */}
+      <TodoList/>
     </div>
   );
 }
