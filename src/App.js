d348b551144db88b1ffd00feb74399f4f663b860
unknown <muskanmujawar7030@gmail.com>
2023-02-15 11:09:24 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Home } from './component3/Home';
 import { TodoCompo } from './TodoCompo';
 
 function App() {
   return (
     <div className="App">
       <TodoCompo />
+      <Home/>
       <h1>This is h1 tag</h1>
     </div>
   );
