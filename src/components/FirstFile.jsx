7cc40045ef71a6a8d53850d7385b66bd50a7c4cb
musaddiqshaheer <musaddiqs784@gmail.com>
2023-02-18 15:24:16 +0530

    new add

--- src/components/FirstFile.jsx
+++ src/components/FirstFile.jsx
@@ -5,6 +5,7 @@ export const FirstFile = () => {
         <div>
             <h1>Musaddiq Siddiqui</h1>
             <h2>Class Project</h2>
+            <h3>This is h3</h3>
         </div>
     );
 };
