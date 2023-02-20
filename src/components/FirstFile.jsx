b088f320cb1fae1843ff0081e04676a7c29c63a3
musaddiqshaheer <musaddiqs784@gmail.com>
2023-02-20 11:20:29 +0530

    my changes

--- src/components/FirstFile.jsx
+++ src/components/FirstFile.jsx
@@ -5,7 +5,7 @@ export const FirstFile = () => {
         <div>
             <h1>Musaddiq Siddiqui</h1>
             <h2>Class Project</h2>
-            <h3>This is h3</h3>
+            <h3>All Component Avalabile</h3>
         </div>
     );
 };
